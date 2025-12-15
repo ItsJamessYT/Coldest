ce1986a308107ee28d6d9945e3d492622e6a639f
James <murganaleandro@gmail.com>
2025-12-15 19:01:35 -0300

    Update Helper.lua

--- Helper.lua
+++ Helper.lua
@@ -6,6 +6,10 @@
 | |__| |_| | |___| |_| | |  _  | |_| | |_) |
  \____\___/|_____|____/  |_| |_|\___/|____/ 
 
+Nice try skid, but this script its portected with 3 obf and make it better with Junkie 📍
+Current Version: 1.6
+Hope you enjoy and join the discord .gg/a4meRSuHgX
+
 ]]
 
 loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/593d1560b9626046a4a9ca0d40d532f158e3d5afa23f9a1d243d202814ab4033/download"))()
